20721591611d92420ed28eb6885636294436c59b
Erhan TUNCEL <yazilim.erhan@gmail.com>
2020-01-04 23:19:45 +0300

    Added: Language changing functionality. Also added turkish and english language files in json format.

--- src/webui/src/app/app.component.ts
+++ src/webui/src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(translate: TranslateService) {
-    translate.setDefaultLang('tr');
-    translate.use('tr');
+  constructor(public translateService: TranslateService,
+              private titleService: Title) {
+    this.translateService.addLangs(['tr', 'en']);
+    let selectedLanguage = localStorage.getItem('selectedLanguage');
+    if (selectedLanguage == null) {
+      const browserLang = this.translateService.getBrowserLang();
+      const languageToUse = browserLang.match(/tr|en/) ? browserLang : 'tr';
+      localStorage.setItem('selectedLanguage', languageToUse);
+      selectedLanguage = languageToUse;
+    }
+    translateService.setDefaultLang(selectedLanguage);
+    translateService.use(selectedLanguage);
+  }
+
+  ngOnInit() {
+    this.translateService.get('BROWSER.title').subscribe((res: string) => {
+      this.titleService.setTitle(res);
+    });
   }
 }
--- src/webui/src/app/layout/footer/footer.component.ts
+++ src/webui/src/app/layout/footer/footer.component.ts
@@ -1,4 +1,6 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService,
+              public titleService: Title) {
+  }
 
   ngOnInit() {
   }
 
+  changeLanguage(language: string) {
+    this.translate.setDefaultLang(language);
+    this.translate.use(language);
+    this.translate.get('BROWSER.title').subscribe((res: string) => {
+      this.titleService.setTitle(res);
+    });
+    localStorage.setItem('selectedLanguage', language);
+
+  }
 }
