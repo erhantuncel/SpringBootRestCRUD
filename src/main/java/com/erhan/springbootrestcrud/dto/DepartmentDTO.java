735a040af31e72075ea2296a12f5b4d42f5d176f
Erhan TUNCEL <yazilim.erhan@gmail.com>
2020-01-28 01:01:34 +0300

    Added: Staff count column to department list table.

--- src/main/java/com/erhan/springbootrestcrud/dto/DepartmentDTO.java
+++ src/main/java/com/erhan/springbootrestcrud/dto/DepartmentDTO.java
@@ -1,8 +1,12 @@
 package com.erhan.springbootrestcrud.dto;
 
 
+import java.util.Set;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+
+import com.erhan.springbootrestcrud.model.Staff;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -23,6 +27,8 @@ public class DepartmentDTO {
 	@Size(max = 100)
 	@ApiModelProperty(value = "Department Name", required = true)
 	private String departmentName;
+	
+	private Set<Staff> staffList;	
 	
 	public DepartmentDTO(String departmentName) {
 		this.departmentName = departmentName;
