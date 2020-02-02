c259c4bb4817ec1e3893dd064101a1a1e534dc2d
Erhan TUNCEL <yazilim.erhan@gmail.com>
2020-02-02 14:20:43 +0300

    Added: Search fields on staff page.

--- src/webui/src/app/pages/staff/staff.component.ts
+++ src/webui/src/app/pages/staff/staff.component.ts
@@ -1,7 +1,9 @@
+import { TranslateService } from '@ngx-translate/core';
 import { PaginationInstance } from 'ngx-pagination';
 import { Component, OnInit, Input } from '@angular/core';
 import { StaffService } from './../../services/shared/staff.service';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-staff',
@@ -14,6 +16,7 @@ export class StaffComponent implements OnInit {
   data = [];
   pageSize = '5';
   pageSizeOptions = ['5', '10', '15'];
+  keywordMinLength = 3;
 
   @Input() pageSizeSelect: string;
 
@@ -23,30 +26,62 @@ export class StaffComponent implements OnInit {
     totalItems: 0
   };
 
+  queryParams = {
+    size: this.config.itemsPerPage,
+    sort: 'id',
+  };
+
+  searchTypes = ['firstName', 'lastName'];
+  searchTypeSelectValue: string;
+  keywordInputValue: string;
+
   constructor(private route: ActivatedRoute,
-              private staffService: StaffService) { }
+              private staffService: StaffService,
+              private toastr: ToastrService,
+              private translate: TranslateService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.departmentId = params.id;
     });
+    this.searchTypeSelectValue = this.searchTypes[0];
     this.config.itemsPerPage = Number(this.pageSize);
     this.getPage(1);
   }
 
   getPage(page: number) {
-    this.staffService
-        .getAllWithDepartmentIdPaginated(this.departmentId, {page: page - 1, size: this.config.itemsPerPage, sort: 'id'})
-        .subscribe(response => {
-      console.log(response);
-      this.data = response.content;
-      this.config.currentPage = page;
-      this.config.totalItems = response.totalElements;
+    this.queryParams[String('page')] = page - 1;
+    if (this.keywordInputValue != null && this.keywordInputValue !== '') {
+      for (const searchType of this.searchTypes) {
+        if (this.queryParams.hasOwnProperty(searchType)) {
+          delete this.queryParams[searchType];
+        }
+      }
+      this.queryParams[this.searchTypeSelectValue] = this.keywordInputValue;
+    } else {
+      delete this.queryParams[this.searchTypeSelectValue];
+    }
+    this.staffService.getAllWithDepartmentIdPaginated(this.departmentId, this.queryParams).subscribe(response => {
+      if (response.status === 200) {
+        console.log(response);
+        this.data = response.body.content;
+        this.config.currentPage = page;
+        this.config.totalItems = response.body.totalElements;
+      } else {
+        this.translate.get('STAFF.toastr.staff.not.found').subscribe(getStaffPageResponse => {
+          this.toastr.error(getStaffPageResponse);
+        });
+      }
     });
   }
 
   onPageSizeSelected(selectedValue: any) {
     this.config.itemsPerPage = selectedValue;
     this.getPage(1);
+  }
+
+  onSubmitSearchForm() {
+    console.log('Search Type = ' + this.searchTypeSelectValue);
+    console.log('keyWord = ' + this.keywordInputValue);
   }
 }
